9411fd4f4418c9da79ff53259622256f953146bf
Craig Buchanan <craig.buchanan@cogniza.com>
2020-09-24 08:07:18 -0500

    initial commit

---
+++ psakefile.ps1
@@ -0,0 +1,15 @@
+FormatTaskName "-------- {0} --------"
+
+Task default -depends Publish
+
+Task Symlink {
+    $Module='PsIntacct'
+    $Here = Get-Location
+    Push-Location ~/.local/share/powershell/Modules
+    ln -s "$Here/$Module" $Module
+    Pop-Location
+}
+  
+Task Publish {
+    publish-module -name ./PsDossier -Repository Lorenz
+}

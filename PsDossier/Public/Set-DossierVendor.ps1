9ff6a606f23a654bc2c6c6f26ad0c05cd49cee7f
Craig Buchanan <craig.buchanan@cogniza.com>
2020-10-08 08:55:04 -0500

    revisions

--- PsDossier/Public/Set-DossierVendor.ps1
+++ PsDossier/Public/Set-DossierVendor.ps1
@@ -2,18 +2,42 @@ function Set-DossierVendor {
     
     [CmdletBinding()]
     param (
-        
+        [Parameter(Mandatory)]
+        [string]$ServerInstance,
+
+        [Parameter()]
+        [string]$Database = 'Dossier',
+
+        [Parameter(Mandatory)]
+        [pscredential]$Credential,
+
+        [Parameter()]
+        [string]$Name,
+
+        [Parameter()]
+        [string]$VendorNumber
     )
-    
-    begin {
-        
+
+    # make a copy of Dictionary
+    $Fields = @{} + $PSBoundParameters
+
+    # remove the information
+    $Fields.Remove('ServerInstance')
+    $Fields.Remove('Database')
+    $Fields.Remove('Credential')
+
+    $Predicate = [pscustomobject]@{
+        UPDATE = "UPDATE  $Database..Vendor"
+        SET = @()
     }
-    
-    process {
-        ID
-    }
-    
-    end {
-        
-    }
+
+    if ( $Name ) { $Predicate.SET += "Name = '$Name'" }
+    if ( $VendorNumber ) { $Predicate.SET += "VendorNumber = '$VendorNumber'" }
+    $Predicate.SET += "audit_ModifiedDate = GetDate()"
+
+    $Query = $Predicate.PsObject.Properties.Value -join "`r`n"
+    Write-Debug $Query
+
+    Invoke-Sqlcmd -Query $Query -ServerInstance $ServerInstance -Database $Database -Credential $Credential
+
 }
--- Tests/Public/Set-DossierVendor.Tests.ps1
+++ Tests/Public/Set-DossierVendor.Tests.ps1
@@ -14,7 +14,78 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . (Join-Path $PublicPath $sut)
 
 Describe "Set-DossierVendor" -Tag 'unit' {
-    It "does something useful" {
-        $false | Should -Be $true
+
+    BeforeAll {
+        # create a PsCredential
+        $SecureString = ConvertTo-SecureString 'Password' -AsPlainText -Force
+        $Credential = New-Object System.Management.Automation.PSCredential ('UserName', $SecureString)
+
+        $Expected = @{
+            ServerInstance = '0.0.0.0'
+            Database = 'Dossier'
+            Credential = $Credential    
+        }
+
+        $PSDefaultParameterValues['Set-DossierVendor:ServerInstance'] = $Expected.ServerInstance
+        $PSDefaultParameterValues['Set-DossierVendor:Database'] = $Expected.Database
+        $PSDefaultParameterValues['Set-DossierVendor:Credential'] = $Expected.Credential   
     }
+
+    Context "Parameter Validation" {
+        BeforeAll {
+            $Command = Set-Command "Set-DossierVendor"
+        }
+
+        $Parameters = @(
+            @{Name='ServerInstance';Type='string';Mandatory=$true}
+            @{Name='Database';Type='string';Mandatory=$false}
+            @{Name='Credential';Type='pscredential';Mandatory=$true}
+        )
+
+        it 'is a <Type>' -TestCases $Parameters {
+            param($Name, $Type, $Mandatory)
+          
+            $Command | Should -HaveParameter $Name -Type $type
+        }
+
+        it 'mandatory is <Mandatory>' -TestCases $Parameters {
+            param($Name, $Type, $Mandatory)
+          
+            if ($Mandatory) { $Command | Should -HaveParameter $Name -Mandatory }
+            else { $Command | Should -HaveParameter $Name -Not -Mandatory }
+        }
+    
+    }
+
+    Context "Database connection" {
+        BeforeEach {
+            # arrange
+            Mock Invoke-Sqlcmd {}
+
+            # act
+            Set-DossierVendor
+        }
+
+        It "uses the specified ServerInstance" {
+            # assert
+            Assert-MockCalled Invoke-Sqlcmd -ParameterFilter {
+                $ServerInstance -eq $Expected.ServerInstance
+            }
+        }
+
+        It "uses the specified Database" {
+            # assert
+            Assert-MockCalled Invoke-Sqlcmd -ParameterFilter {
+                $Database -eq $Expected.Database
+            }
+        }
+
+        It "uses the specified Credentail" {
+            # assert
+            Assert-MockCalled Invoke-Sqlcmd -ParameterFilter {
+                $Credentail -eq $Expected.Credentail
+            }
+        }
+    }
+
 }

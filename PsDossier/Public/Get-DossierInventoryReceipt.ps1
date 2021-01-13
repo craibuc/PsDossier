298ad881a96dc41777d435abd05c5f7789b6b505
Craig Buchanan <craig.buchanan@cogniza.com>
2021-01-13 15:39:56 -0600

    renaming Get-DossierInventoryReceipt adding Status, VendorNumber, and InvoiceNumber parameters enhancing query creating nested graph

---
+++ PsDossier/Public/Get-DossierInventoryReceipt.ps1
@@ -0,0 +1,122 @@
+<#
+.SYNOPSIS
+
+.PARAMETER ServerIntance
+The SQL Server instance.
+
+.PARAMETER Database
+The SQL Server database.
+
+.PARAMETER Credential
+The SQL Server credentials.
+
+.PARAMETER Number
+Get a Bill by its `VendorNumber`.
+
+#>
+function Get-DossierInventoryReceipt {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$ServerInstance,
+
+        [Parameter()]
+        [string]$Database = 'Dossier',
+
+        [Parameter(Mandatory)]
+        [pscredential]$Credential,
+
+        [Parameter()]
+        [Nullable[DateTime]]$FromDate,
+
+        [Parameter()]
+        [Nullable[DateTime]]$ToDate,
+
+        [Parameter()]
+        [ValidateSet('OPEN','CLOSED')]
+        [string]$Status = 'CLOSED',
+
+        [Parameter()]
+        [string]$VendorNumber,
+
+        [Parameter()]
+        [string]$InvoiceNumber
+    )
+    
+    $Predicate = [pscustomobject]@{
+        SELECT = 
+            "SELECT  IADOC.ID, IADOC.[Type], IADOC.Status, IADOC.AuthorizationNumber, IADOC.InvoiceNumber
+                ,IADOC.DocDate, IADOC.OkToPayDate
+                ,IADOC.Notes, IADOC.TotalTax
+                ,V.VendorNumber, V.Name VendorName
+                ,PO.PONumber
+                ,BM.Name BillingMethod
+                ,S.Name SiteName
+                ,IADTL.Quantity
+                ,CAST(IADTL.PerUnitCost AS numeric(18,2)) PerUnitCost
+                ,CAST(IADTL.PerUnitCredit AS numeric(18,2)) PerUnitCredit
+                ,P.[Description] PartDescription, P.PartNumber, P.Tire
+            FROM    $Database..InventoryAdjustmentDocument IADOC 
+            INNER JOIN Dossier..InventoryAdjustmentDetail as IADTL ON IADOC.ID = IADTL.InvAdjDocID
+            INNER JOIN Dossier..Part P ON IADTL.PartID = P.ID
+            LEFT OUTER JOIN Dossier..Site S on IADOC.SiteID = S.ID
+            LEFT OUTER JOIN Dossier..BillingMethod BM on IADOC.BillingMethodID = BM.ID
+            LEFT OUTER JOIN Dossier..PurchaseOrder PO ON IADOC.POID = PO.ID
+            LEFT OUTER JOIN Dossier..Vendor V ON IADOC.VendorID = V.ID"
+        WHERE = "WHERE 1=1 AND IADOC.OkToPay=1"
+        ORDER = "ORDER BY VendorNumber, InvoiceNumber"
+    }
+
+    if ( $FromDate ) { $Predicate.WHERE += "`r`nAND IADOC.OkToPayDate >= '$FromDate'" }
+    if ( $ToDate ) { $Predicate.WHERE += "`r`nAND IADOC.OkToPayDate <= '$ToDate'" }
+    if ( $Status ) { $Predicate.WHERE += "`r`nAND IADOC.Status = '$Status'" }
+    if ( $VendorNumber ) { $Predicate.WHERE += "`r`nAND V.VendorNumber = '$VendorNumber'" }
+    if ( $InvoiceNumber ) { $Predicate.WHERE += "`r`nAND IADOC.InvoiceNumber = '$InvoiceNumber'" }
+
+    $Query = $Predicate.PsObject.Properties.Value -join "`r`n"
+    Write-Debug $Query
+
+    Invoke-Sqlcmd -Query $Query -ServerInstance $ServerInstance -Database $Database -Credential $Credential | Group-Object -Property VendorNumber,InvoiceNumber | ForEach-Object {
+
+        $VendorNumber,$InvoiceNumber = $_.Name -split ', '
+
+        $Message = "Processing Vendor #$VendorNumber/Invoice #$InvoiceNumber..."
+        Write-Debug $Message
+
+        # create object w/ desired graph
+        $InventoryAdjustment = [pscustomobject]@{
+            ID = $_.Group[0].ID
+            DocDate = $_.Group[0].DocDate
+            OkToPayDate = $_.Group[0].OkToPayDate
+            Type = $_.Group[0].Type
+            Status = $_.Group[0].Status
+            VendorNumber = $_.Group[0].VendorNumber
+            VendorName = $_.Group[0].VendorName
+            InvoiceNumber = $_.Group[0].InvoiceNumber
+            PONumber = $_.Group[0].PONumber
+            Notes = $_.Group[0].Notes | nz
+            TotalTax = $_.Group[0].TotalTax
+            InventoryAdjustmentDetails = @()
+        } 
+
+        # add all lineitems
+        $_.Group | ForEach-Object {
+
+            $InventoryAdjustment.InventoryAdjustmentDetails += [pscustomobject]@{
+                CostType = $_.CostType
+                Quantity = $_.Quantity
+                PerUnitCost = $_.PerUnitCost
+                PerUnitCredit = $_.PerUnitCredit
+                PartDescription = $_.PartDescription | nz
+                PartNumber = $_.PartNumber
+                Tire = [bool]$_.Tire
+            }
+
+        }
+
+        $InventoryAdjustment
+
+    }
+
+}
---
+++ Tests/Public/Get-DossierInventoryReceipt.Tests.ps1
@@ -0,0 +1,192 @@
+BeforeAll {
+
+    # /PsDossier
+    $ProjectDirectory = Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Parent
+    $PublicPath = Join-Path $ProjectDirectory "/PsDossier/Public/"
+
+    $SUT = (Split-Path -Leaf $PSCommandPath) -replace '\.Tests\.', '.'
+    . (Join-Path $PublicPath $SUT)
+
+}
+
+Describe "Get-DossierInventoryReceipt" -Tag 'unit' {
+
+    Context "Parameter Validation" {
+
+        BeforeAll {
+            $Command = Get-Command "Get-DossierInventoryReceipt"
+        }
+
+        $Parameters = @(
+            @{Name='ServerInstance';Type='string';Mandatory=$true}
+            @{Name='Database';Type='string';Mandatory=$false}
+            @{Name='Credential';Type='pscredential';Mandatory=$true}
+            @{Name='FromDate';Type='[nullable[datetime]]';Mandatory=$false}
+            @{Name='ToDate';Type='[nullable[datetime]]';Mandatory=$false}
+            @{Name='Status';Type='string';Mandatory=$false}
+            @{Name='VendorNumber';Type='string';Mandatory=$false}
+            @{Name='InvoiceNumber';Type='string';Mandatory=$false}
+        )
+
+        Context 'Type' {
+            it '<Name> is a <Type>' -TestCases $Parameters {
+                param($Name, $Type, $Mandatory)
+              
+                $Command | Should -HaveParameter $Name -Type $type
+            }    
+        }
+
+        Context 'Type' {
+            it '<Name> mandatory is <Mandatory>' -TestCases $Parameters {
+                param($Name, $Type, $Mandatory)
+              
+                if ($Mandatory) { $Command | Should -HaveParameter $Name -Mandatory }
+                else { $Command | Should -HaveParameter $Name -Not -Mandatory }
+            }    
+        }
+    
+    }
+
+    Context "Usage" {
+
+        BeforeAll {
+            # create a PsCredential
+            $SecureString = ConvertTo-SecureString 'Password' -AsPlainText -Force
+            $Credential = New-Object System.Management.Automation.PSCredential ('UserName', $SecureString)
+    
+            $Expected = @{
+                ServerInstance = '0.0.0.0'
+                Database = 'Dossier'
+                Credential = $Credential    
+            }
+    
+            $PSDefaultParameterValues['Get-DossierInventoryReceipt:ServerInstance'] = $Expected.ServerInstance
+            $PSDefaultParameterValues['Get-DossierInventoryReceipt:Database'] = $Expected.Database
+            $PSDefaultParameterValues['Get-DossierInventoryReceipt:Credential'] = $Expected.Credential   
+        }
+    
+        BeforeEach {
+            # arrange
+            Mock Invoke-Sqlcmd {}
+
+            # act
+            Get-DossierInventoryReceipt
+        }
+
+        It "uses the specified ServerInstance" {
+            # assert
+            Assert-MockCalled Invoke-Sqlcmd -ParameterFilter {
+                $ServerInstance -eq $Expected.ServerInstance
+            }
+        }
+
+        It "uses the specified Database" {
+            # assert
+            Assert-MockCalled Invoke-Sqlcmd -ParameterFilter {
+                $Database -eq $Expected.Database
+            }
+        }
+
+        It "uses the specified Credentail" {
+            # assert
+            Assert-MockCalled Invoke-Sqlcmd -ParameterFilter {
+                $Credentail -eq $Expected.Credentail
+            }
+        }
+    }
+
+    Context "FromDate" {
+
+        It "adds a FromDate filter to the WHERE clause" {
+            # arrange
+            $FromDate = '9/1/2020'
+            Mock Invoke-Sqlcmd {}
+
+            # act
+            Get-DossierInventoryReceipt -FromDate $FromDate
+
+            # assert
+            Should -Invoke Invoke-Sqlcmd -ParameterFilter {
+                Write-Debug $query 
+
+                $Query -like "*AND IADOC.OkToPayDate >= '$( ([datetime]$FromDate).ToString('MM/dd/yyyy HH:mm:ss') )'*"
+            }
+        }
+
+    }
+
+    Context "ToDate" {
+
+        It "adds a ToDate filter to the WHERE clause" {
+            # arrange
+            $ToDate = '9/1/2020'
+            Mock Invoke-Sqlcmd {}
+
+            # act
+            Get-DossierInventoryReceipt -ToDate $ToDate
+
+            # assert
+            Should -Invoke Invoke-Sqlcmd -ParameterFilter {
+                Write-Debug $query 
+
+                $Query -like "*AND IADOC.OkToPayDate <= '$( ([datetime]$ToDate).ToString('MM/dd/yyyy HH:mm:ss') )'*"
+            }
+        }
+
+    }
+
+    Context "Status" {
+
+        It "adds a Status filter to the WHERE clause" {
+            # arrange
+            $Status = 'CLOSED'
+            Mock Invoke-Sqlcmd {}
+
+            # act
+            Get-DossierInventoryReceipt -Status $Status
+
+            # assert
+            Should -Invoke Invoke-Sqlcmd -ParameterFilter {
+                $Query -like "*AND IADOC.Status = '$Status'*"
+            }
+        }
+
+    }
+
+    Context "VendorNumber" {
+
+        It "adds a VendorNumber filter to the WHERE clause" {
+            # arrange
+            $VendorNumber = 'ABC123'
+            Mock Invoke-Sqlcmd {}
+
+            # act
+            Get-DossierInventoryReceipt -VendorNumber $VendorNumber
+
+            # assert
+            Should -Invoke Invoke-Sqlcmd -ParameterFilter {
+                $Query -like "*AND V.VendorNumber = '$VendorNumber'*"
+            }
+        }
+
+    }
+
+    Context "InvoiceNumber" {
+
+        It "adds a InvoiceNumber filter to the WHERE clause" {
+            # arrange
+            $InvoiceNumber = 'INV-1234'
+            Mock Invoke-Sqlcmd {}
+
+            # act
+            Get-DossierInventoryReceipt -InvoiceNumber $InvoiceNumber
+
+            # assert
+            Should -Invoke Invoke-Sqlcmd -ParameterFilter {
+                $Query -like "*AND IADOC.InvoiceNumber = '$InvoiceNumber'*"
+            }
+        }
+
+    }
+
+}
